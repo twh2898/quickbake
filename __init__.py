07caeec22ef6c66bda1cf7bfad8378ff11bcf8fa
Thomas Harrison <twh2898@vt.edu>
2023-09-26 21:43:55 -0400

    Added image save code

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import os
 import bpy
 
 bl_info = {
@@ -25,6 +26,19 @@ class QuickBakeToolPropertyGroup(bpy.types.PropertyGroup):
         default=True
     )
 
+    save_img: bpy.props.BoolProperty(  # type: ignore
+        name="Save Images",
+        description="Write images to file after baking",
+        default=False
+    )
+
+    image_path: bpy.props.StringProperty(  # type: ignore
+        name="Texture Path",
+        description="Directory for baking output",
+        default='',
+        subtype='DIR_PATH'
+    )
+
     bake_name: bpy.props.StringProperty(  # type: ignore
         name="Name",
         description="Name used fot the baked texture images",
@@ -35,6 +49,72 @@ class QuickBakeToolPropertyGroup(bpy.types.PropertyGroup):
         name="UV",
         description="Name used fot the uv bake layer",
         default="bake_uv"
+    )
+
+    diffuse_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Diffuse",
+        description="Bake the diffuse map",
+        default=True
+    )
+
+    normal_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Normal",
+        description="Bake the normal map",
+        default=True
+    )
+
+    roughness_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Roughness",
+        description="Bake the roughness map",
+        default=True
+    )
+
+    ao_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Ao",
+        description="Bake the Ao map",
+        default=False
+    )
+
+    shadow_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Shadow",
+        description="Bake the Shadow map",
+        default=False
+    )
+
+    position_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Position",
+        description="Bake the Position map",
+        default=False
+    )
+
+    uv_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Uv",
+        description="Bake the Uv map",
+        default=False
+    )
+
+    emit_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Emit",
+        description="Bake the Emit map",
+        default=False
+    )
+
+    environment_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Environment",
+        description="Bake the Environment map",
+        default=False
+    )
+
+    glossy_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Glossy",
+        description="Bake the Glossy map",
+        default=False
+    )
+
+    transmission_enabled: bpy.props.BoolProperty(  # type: ignore
+        name="Transmission",
+        description="Bake the Transmission map",
+        default=False
     )
 
 
@@ -110,41 +190,71 @@ class QuickBake_OT_bake(bpy.types.Operator):
             self.report({'ERROR'}, 'Active object must be a mesh')
             return {'CANCELLED'}
 
-        bake_nodes = self.setup_nodes(obj)
-
         props = context.scene.QuickBakeToolPropertyGroup
 
-        for pass_type in ['DIFFUSE', 'NORMAL', 'ROUGHNESS']:
+        bake_nodes = self.setup_nodes(obj)
+        bake_uv = self.setup_uv(obj, props.bake_uv)
+
+        passes = []
+        if props.diffuse_enabled:
+            passes.append('DIFFUSE')
+        if props.normal_enabled:
+            passes.append('NORMAL')
+        if props.roughness_enabled:
+            passes.append('ROUGHNESS')
+        if props.ao_enabled:
+            passes.append('AO')
+        if props.shadow_enabled:
+            passes.append('SHADOW')
+        if props.position_enabled:
+            passes.append('POSITION')
+        if props.uv_enabled:
+            passes.append('UV')
+        if props.emit_enabled:
+            passes.append('EMIT')
+        if props.environment_enabled:
+            passes.append('ENVIRONMENT')
+        if props.glossy_enabled:
+            passes.append('GLOSSY')
+        if props.transmission_enabled:
+            passes.append('TRANSMISSION')
+
+        for pass_type in passes:
             img = self.setup_image(obj,
                                    bake_nodes,
                                    props.bake_name,
                                    pass_type.lower(),
                                    props.reuse_tex)
 
-            # image_name = obj.name + '_' + props.bake_name
-            # img = bpy.data.images.get(image_name)
-            # if img is None or props.reuse_tex:
-            #     img = bpy.data.images.new(image_name, 1024, 1024)
-
-            # for node in bake_nodes:
-            #     node.image = img
-
-            bake_uv = self.setup_uv(obj, props.bake_uv)
-
             self.report({'INFO'}, 'Baking pass %s' % pass_type)
 
             bpy.context.view_layer.objects.active = obj
+            save_mode = 'INTERNAL'
+            filepath = ''
+            if props.save_img:
+                save_mode = 'EXTERNAL'
+                filepath = os.path.join(props.image_path, img.name + '.png')
+
             bpy.ops.object.bake(type=pass_type,
                                 pass_filter={'COLOR'},
-                                save_mode='EXTERNAL',
-                                uv_layer='bake_uv')
+                                uv_layer='bake_uv',
+                                save_mode=save_mode,
+                                filepath=filepath,
+                                )
 
-            # img.save_render(filepath='/tmp/baked.png')
+            # if props.save_img:
+            #     filepath = os.path.join(props.image_path, img.name + '.png')
+            #     self.report({'INFO'}, 'Baking cwd is %s' % os.getcwd())
+            #     self.report({'INFO'}, 'Baking path is %s' % filepath)
+            #     img.filepath_raw = filepath
+            #     img.file_format = 'PNG'
+            #     img.save_render(filepath=filepath)
 
         self.report({'INFO'}, 'Baking complete')
 
         if props.clean_up:
             self.cleanup_nodes(obj)
+            obj.data.uv_layers.remove(bake_uv)
 
         return {'FINISHED'}
 
@@ -176,11 +286,58 @@ class QuickBake_PT_main(bpy.types.Panel):
         row = layout.row()
         row.prop(props, "bake_uv")
 
+        layout.separator()
+        layout.label(text='Options')
         row = layout.row()
         row.prop(props, "reuse_tex")
 
         row = layout.row()
         row.prop(props, "clean_up")
+
+        layout.separator()
+        layout.label(text='Output')
+        row = layout.row()
+        row.prop(props, "save_img")
+
+        row = layout.row()
+        row.prop(props, "image_path")
+
+        layout.separator()
+        layout.label(text='Layers')
+        row = layout.row()
+        row.prop(props, 'diffuse_enabled')
+
+        row = layout.row()
+        row.prop(props, 'normal_enabled')
+
+        row = layout.row()
+        row.prop(props, 'roughness_enabled')
+
+        row = layout.row()
+        row.prop(props, 'ao_enabled')
+
+        row = layout.row()
+        row.prop(props, 'shadow_enabled')
+
+        row = layout.row()
+        row.prop(props, 'position_enabled')
+
+        row = layout.row()
+        row.prop(props, 'uv_enabled')
+
+        row = layout.row()
+        row.prop(props, 'emit_enabled')
+
+        row = layout.row()
+        row.prop(props, 'environment_enabled')
+
+        row = layout.row()
+        row.prop(props, 'glossy_enabled')
+
+        row = layout.row()
+        row.prop(props, 'transmission_enabled')
+
+        row = layout.row()
 
 
 def register():
