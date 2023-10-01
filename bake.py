177807224ba284db77203b862ac113ef308130c6
Thomas Harrison <twh2898@vt.edu>
2023-10-01 14:35:18 -0400

    Split helper functions into files

---
+++ bake.py
@@ -0,0 +1,94 @@
+"""Baking helper functions."""
+
+import bpy
+
+import logging
+
+_l = logging.getLogger(__name__)
+
+
+def setup_bake_nodes(obj):
+    '''Create material nodes required for baking.'''
+    _l.info('Creating bake nodes for object %s', obj.name)
+
+    bake_nodes = []
+    for mat in obj.data.materials:
+        _l.debug('Creating nodes for material %s', mat.name)
+
+        mat.use_nodes = True
+        nodes = mat.node_tree.nodes
+        texture_node = nodes.new('ShaderNodeTexImage')
+        texture_node.name = 'Bake_node'
+        texture_node.select = True
+        nodes.active = texture_node
+        bake_nodes.append(texture_node)
+
+    return bake_nodes
+
+
+def cleanup_bake_nodes(obj):
+    '''Remove material nodes created for baking by setup_bake_nodes.'''
+    _l.info('Cleaning up bake nodes for object %s', obj.name)
+
+    for mat in obj.data.materials:
+        _l.debug('Clean up nodes for material %s', mat.name)
+
+        for n in mat.node_tree.nodes:
+            if n.name == 'Bake_node':
+                _l.debug('Remove bake node %s', n.name)
+                mat.node_tree.nodes.remove(n)
+
+
+def setup_bake_uv(obj, name):
+    '''Create a uv layer to unwrap obj for baking.'''
+    _l.info('Creating uv layer %s for baking', name)
+
+    def unwrap_uv(obj, uv):
+        _l.info('Unwrapping object %s to layer %s', obj.name, uv.name)
+
+        active_layer = None
+        for layer in obj.data.uv_layers:
+            if layer.active:
+                _l.debug('Found active layer %s', layer.name)
+                active_layer = layer
+                break
+
+        uv.active = True
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_all(action='SELECT')
+        bpy.ops.uv.smart_project(island_margin=0.001)
+        bpy.ops.object.mode_set(mode='OBJECT')
+        uv.active = False
+
+        if active_layer is not None:
+            _l.debug('Restoring active layer %s', active_layer.name)
+            active_layer.active = True  # type: ignore
+
+    bake_uv = obj.data.uv_layers.get(name)
+    if bake_uv is None:
+        bake_uv = obj.data.uv_layers.new(name=name)
+        unwrap_uv(obj, bake_uv)
+
+    else:
+        _l.debug('Using existing uv layer')
+
+    return bake_uv
+
+
+def setup_bake_image(obj, bake_nodes, bake_name, pass_name, reuse_tex, is_data=False):
+    _l.info('Creating image for baking object %s', obj.name)
+
+    image_name = obj.name + '_' + bake_name + '_' + pass_name
+    _l.debug('Image name %s', image_name)
+
+    img = bpy.data.images.get(image_name)
+    if img is None or not reuse_tex:
+        img = bpy.data.images.new(image_name, 1024, 1024, is_data=is_data)
+
+    else:
+        _l.debug('Using existing image')
+
+    for node in bake_nodes:
+        node.image = img
+
+    return img
---
+++ material.py
@@ -0,0 +1,70 @@
+"""Material helper functions."""
+
+import bpy
+from bpy_extras.node_shader_utils import PrincipledBSDFWrapper
+
+import logging
+
+_l = logging.getLogger(__name__)
+
+
+
+def setup_bake_material(obj, name, bake_uv_name, diffuse, roughness, normal):
+    _l.info('Creating material %s for object %s', name, obj.name)
+
+    mat = bpy.data.materials.get(name)
+    if mat is not None:
+        _l.debug('Found existing material, skipping')
+        return mat
+
+    mat = bpy.data.materials.new(name=name)
+    mat.use_nodes = True
+    obj.data.materials.append(mat)
+
+    principled_mat = PrincipledBSDFWrapper(mat, is_readonly=False)
+    principled_mat.roughness = 1.0
+
+    principled_node = principled_mat.node_principled_bsdf
+
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+
+    uv_node = nodes.new(type='ShaderNodeUVMap')
+    uv_node.uv_map = bake_uv_name
+    uv_node.location.x -= 1200
+    # uv_node.location.y += 300
+
+    mapping_node = nodes.new(type='ShaderNodeMapping')
+    mapping_node.location.x -= 1000
+    # mapping_node.location.y += 300
+    links.new(uv_node.outputs['UV'], mapping_node.inputs['Vector'])
+
+    def make_tex_node(img, y):
+        tex_node = nodes.new(type='ShaderNodeTexImage')
+        tex_node.image = img
+        tex_node.location.x -= 600
+        tex_node.location.y += y
+
+        # links.new(mapping_node.outputs[0], tex_node.inputs['Vector'])
+        links.new(mapping_node.outputs['Vector'], tex_node.inputs['Vector'])
+
+        # TODO: color space if not set by default
+        # tex_node.image.colorspace_settings.name = '...'
+
+        return tex_node
+
+    diff_node = make_tex_node(diffuse, 400)
+    links.new(diff_node.outputs['Color'], principled_node.inputs['Base Color'])
+
+    rough_node = make_tex_node(roughness, 100)
+    links.new(rough_node.outputs['Color'], principled_node.inputs['Roughness'])
+
+    norm_node = make_tex_node(normal, -200)
+    norm_map_node = nodes.new(type='ShaderNodeNormalMap')
+    norm_map_node.location.x -= 300
+    links.new(norm_node.outputs['Color'], norm_map_node.inputs['Color'])
+    links.new(norm_map_node.outputs['Normal'],
+              principled_node.inputs['Normal'])
+
+    return mat
+
